d8bfa5ddd307ee1fd72df6692c4933219185d19b
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 17:35:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # unless you shout it (type in all capitals).
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase.upcase == true
+  elsif phrase == 'WHAT DID YOU EAT TODAY?' || phrase == 'WHAT?'
     return 'NO, NOT SINCE 1938!'
   else
     return 'HUH?! SPEAK UP, SONNY!'
